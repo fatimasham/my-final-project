776e8b44fd8ff1b30b0c7d38690edfa2bf759cf3
fatimasham <167670583+fatimasham@users.noreply.github.com>
2024-09-26 09:22:04 +0500

    Update SlideBar.js

--- src/SlideBar.js
+++ src/SlideBar.js
@@ -1,1 +1,27 @@
 
+import React from "react";
+import { slides } from "./slides";
+import { useState } from "react";
+
+const SlideBar = () => {
+    
+const [shop, setShop] = useState(0);
+const {id, image} = slides[shop];
+console.log(slides[shop])
+
+return(<div> 
+    <div>
+    <img src={id} />
+    </div>
+
+    <div>
+    <img src={image} width="400px" alt="accessorize"/>
+    </div>
+   </div>)
+
+
+}
+
+export default SlideBar;
+
+
