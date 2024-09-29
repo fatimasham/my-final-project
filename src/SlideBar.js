8bdad8f31078d345d64199dddbcec8bb8381fe3b
fatimasham <167670583+fatimasham@users.noreply.github.com>
2024-09-29 14:18:39 +0500

    Update SlideBar.js

--- src/SlideBar.js
+++ src/SlideBar.js
@@ -3,25 +3,50 @@ import React from "react";
 import { slides } from "./slides";
 import { useState } from "react";
 
+
 const SlideBar = () => {
-    
-const [shop, setShop] = useState(0);
-const {id, image} = slides[shop];
-console.log(slides[shop])
+
+const [shop, setShop] = useState(3);
+const {id, image, description} = slides[shop];
+
+const previousShop= () => {
+    setShop((shop => {
+        shop --;
+        if (shop < 0) {
+            return slides.length-1;
+        }
+        return shop;
+    }))
+}
+
+const nextShop= () => {
+    setShop((shop =>{
+        shop ++;
+        if (shop > slides.length - 1) {
+            shop = 0;
+        }
+        return shop;
+    }))
+}
 
 return(<div> 
-    <div>
-    <img src={id} />
+
+    <div className="location">
+    <img src={image} width="600px" alt="accessorize"/>
     </div>
 
-    <div>
-    <img src={image} width="400px" alt="accessorize"/>
+    <div className="location">
+    <h6>{id} - {description}</h6>
+    </div>
+
+    <div className="btn location">
+        <button onClick={previousShop}>Previous</button>
+        <button onClick={nextShop}>Next</button>
     </div>
    </div>)
-
-
 }
 
 export default SlideBar;
+
 
 
