578cdda2f21b59b68348dcd00cface0f303e0ba5
fatimasham <167670583+fatimasham@users.noreply.github.com>
2024-09-29 14:19:37 +0500

    Update slides.js

--- src/slides.js
+++ src/slides.js
@@ -1,30 +1,43 @@
+import shopone from "./shopone.jpg"
+import shoptwo from "./shoptwo.jpg"
+import shopthree from "./shopthree.jpg"
+import shopfour from "./shopfour.jpg"
+import Accessorize from "./Accessorize.jpg"
+
 export const slides = [
 
     {
       id: 1,
-      image: "shopone"
+      image: shopone,
+      description: "Accessorize Oxford Street East Closed, Opens at 11:00 · 55-59 Oxford Street, Soho London"
+
     },
   
     {
       id: 2,
-      image: "shoptwo"
+      image: shoptwo,
+      description: "Accessorize Waterloo Closed · Opens at 10:00 · Unit 4, Waterloo Station London"
     },
   
     {
       id: 3,
-      image: "shopthree"
+      image: shopthree,
+      description: "Accessorize Victoria Street Closed · Opens at 11:00 · Unit 4, 92 Victoria Street London"
     },
   
     {
       id: 4,
-      image: "shopfour"
+      image: shopfour,
+      description: "Accessorize Victoria Station Open · Closes at 18:00 · Unit 32 Victoria Station London"
     },
   
     {
       id: 5,
-      image: "Accessorize"
+      image: Accessorize,
+      description: "Accessorize St Pauls Closed · Opens at 12:00 · 18 Cheapside, One New Change London"
     }
   
   ]
+  
   
   
